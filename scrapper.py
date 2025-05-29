a990b8e6096c696d3e7bd6937b852a695064e53d
bijjaarunteja <139552325+bijjaarunteja@users.noreply.github.com>
2025-05-29 10:42:20 +0530

    scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from gst_data import gst_numbers
+from unused import gst_numbers
 
 import time
 import csv
